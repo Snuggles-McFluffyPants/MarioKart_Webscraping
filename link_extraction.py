f59f5456744f606f141b4b54384b692812e3b70a
Snuggles-McFluffyPants <68079565+Snuggles-McFluffyPants@users.noreply.github.com>
2021-07-09 22:48:14 -0400

    Cleaning up: "link_extraction.py"
    Removing commented out lines of code and making a gui with a list of links pop up if this module isn't imported

--- link_extraction.py
+++ link_extraction.py
@@ -7,11 +7,13 @@ final_links_list is another final output that comes in list form
 
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-from string_and_list_opperations import *
+from tkinter import *
 
+from string_and_list_opperations import *
 
 # import the file dropdown menu to select whether your working with drivers, gliders or karts
 from dropdown_menu import selection as working_with
+
 
 driver_links=['https://game8.co/games/mariokarttour/archives/270420',
              'https://game8.co/games/mariokarttour/archives/270419',
@@ -46,20 +48,6 @@ def extraction(webpage):
     for link in soup.findAll('a'):
         links.append(link.get('href'))
 
-    # #Filter out results that come before the string m.me
-    # start = 0
-    # done = 'False'
-    # while done == 'False' and start < len(links):
-    #     if '#hl_3' in links[start]:
-    #         done = 'True'
-    #     start+=1
-    #
-    # links = links[start:-1]
-    #
-    # links = links[0:(start-1)]
-    # seperator = ', '
-    # links = seperator.join(links)
-
     global final_links_list
     final_links_list = final_links_list + links
     return final_links_list
@@ -75,37 +63,11 @@ final_links_list = [x for x in final_links_list if x is not None]
 
 final_links_list = list(filter(lambda x: len(x) >= 40, final_links_list))
 
-
-# # Remove unneeded characters from final_links list
-# bad_chars = ['hl_4,', ',', ' ', '#,','#']
-#
-# final_links_list = \
-#     bad_chars_removal(bad_chars, final_links_list,'')
-#
-# # Changes in final_links_list string to make it itterable
-#
-# final_links_list = final_links_list.replace('https://game8.co/games/','/games/')
-# final_links_list = final_links_list.replace('/games/',', https://game8.co/games/')
-# final_links_list = final_links_list.replace('https:', ', https:')
-#
-# final_links_list = first_letter_filter(final_links_list)
-#
-#
-#
-# #convert final_links_list into a list
-# final_links_list = final_links_list.split(", ")
-
-
-
 #filter out unhelpful results
 final_links_list = \
     str_list_filter('https://game8.co/games/mariokarttour/archives/',final_links_list,'keep')
-# final_links_list = \
-#     str_list_filter('javascript',final_links_list,'remove')
 final_links_list = \
     str_list_filter('comment',final_links_list,'remove')
-
-# print(final_links_list)
 
 # Make links_list_string itterable
 links_list_string = str(final_links_list)
@@ -115,7 +77,6 @@ bad_chars = ['[', ']']
 links_list_string = \
     bad_chars_removal(bad_chars, links_list_string,'')
 
-from tkinter import *
 
 def text_window(string):
     master = Tk()
@@ -133,5 +94,6 @@ def text_window(string):
 
     mainloop()
 
-#text_window(links_list_string)
+if __name__ == "__main__":
+    text_window(links_list_string)
 
